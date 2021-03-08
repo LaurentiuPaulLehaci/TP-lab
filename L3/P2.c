b90bc3db23e5acaff310022dbdd0a9d8f7e05c6e
LaurentiuPaulLehaci <79301643+LaurentiuPaulLehaci@users.noreply.github.com>
2021-03-08 22:37:56 +0200

    Add files via upload

---
+++ L3/P2.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct bit
+{
+	unsigned char *val;
+	struct bit *urm;
+}nod;
+
+nod *adaugare(nod *prim, unsigned char v)
+{
+	nod *p;
+	p = (nod*)malloc(sizeof(nod));
+	p->val= (char*)malloc(sizeof(unsigned char));
+	if(p==NULL || p->val==NULL)
+	{
+		printf("eroare");
+		exit(0);
+	}
+
+	p->val=v;
+	p->urm = NULL;
+	if (prim == NULL)
+		return p;
+	else
+	{
+		p->urm = prim;
+		return p;
+
+	}
+
+
+}
+
+void afisare(nod *prim)
+{
+	nod *q;
+	q = prim;
+	while(q!=NULL)
+	{
+		printf("%d ", q->val);
+		q = q->urm;
+	}
+}
+
+
+
+int main()
+{
+    nod *prim = NULL;
+    int nr;
+    printf("Introduceti numarul:");
+    scanf("%d",&nr);
+    while(nr)
+    {
+        prim=adaugare(prim,nr%2);
+        nr=nr/2;
+    }
+    afisare(prim);
+    return 0;
+}
+
